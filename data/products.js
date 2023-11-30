f65fe4cd9b8f0cee7b7b1354f87470ddaaa310cc
jumokeoladimeji <jumoke5ng@yahoo.com>
2023-11-30 09:44:11 +0100

    feat: add shopping cart
    WIP: stripe integration

---
+++ data/products.js
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    id: 1,
+    name: "Fishing Card",
+    price: 10000,
+    emoji: "⛏️",
+    currency: "USD",
+  },
+  {
+    id: 2,
+    name: "Golf Card",
+    /* Prices are in smallest currency unit, 100 pence = £1 */
+    price: 10000,
+    emoji: "🎤",
+    currency: "USD",
+  }
+];
