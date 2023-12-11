8fe245e504ad1ff1751362a32b614852c0cc18cf
jumokeoladimeji <jumoke5ng@yahoo.com>
2023-12-11 05:06:58 +0100

    fix bug with headers

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,4 +1,4 @@
-import Header from "/components/Header";
+// import Header from "/components/Header";
 import {useRouter} from "next/router";
 
 function ThankYou() {
@@ -6,7 +6,7 @@ function ThankYou() {
     
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
             <div className="container mx-auto">
                 <div className="text-3xl font-bold">
                 <h1>Thank You, your order is being processed...</h1>            
