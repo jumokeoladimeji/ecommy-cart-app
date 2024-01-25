ae9dbba1795afc3fbe4d735f8d85035c2d82109c
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2024-01-25 14:45:06 +0100

    fix checkout api

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -7,92 +7,91 @@ const stripe = require('stripe')(
 
 export default async function (req, res) {
 	console.log(req.method);
-	// if (req.method === 'POST') {
-	// 	res.json('Should be a post request');
-	// 	return;
-	// }
+	if (req.method === 'POST') {
+		try {
+			const {
+				email,
+				name,
+				user_id,
+				address,
+				country,
+				zip,
+				city,
+				cartProducts,
+				customMessage,
+				phone_number,
+				token,
+			} = req.body;
 
-	try {
-		const {
-			email,
-			name,
-			user_id,
-			address,
-			country,
-			zip,
-			city,
-			cartProducts,
-			customMessage,
-			phone_number,
-			token,
-		} = req.body;
+			const line_items = Object.values(cartProducts).map(
+				(item) => ({
+					// id: item.id,
+					quantity: item.quantity,
+					product_data: item.product_data,
+					price_data: item.price_data,
+				}),
+			);
 
-		const line_items = Object.values(cartProducts).map(
-			(item) => ({
-				// id: item.id,
-				quantity: item.quantity,
-				product_data: item.product_data,
-				price_data: item.price_data,
-			}),
-		);
+			const orderData = {
+				line_items,
+				user_id,
+				email,
+				name,
+				address1: address,
+				city,
+				country,
+				zip,
+				customized_message: customMessage,
+				shipping_phone_number: phone_number,
+				paid: false,
+			};
 
-		const orderData = {
-			line_items,
-			user_id,
-			email,
-			name,
-			address1: address,
-			city,
-			country,
-			zip,
-			customized_message: customMessage,
-			shipping_phone_number: phone_number,
-			paid: false,
-		};
+			const orderDoc = await createOrder(orderData, token);
 
-		const orderDoc = await createOrder(orderData, token);
-
-		const session = await stripe.checkout.sessions.create({
-			line_items,
-			shipping_options: [
+			const session = await stripe.checkout.sessions.create(
 				{
-					shipping_rate_data: {
-						type: 'fixed_amount',
-						fixed_amount: {
-							amount: 1000,
-							currency: 'usd',
-						},
-						display_name: 'Classic Shipping',
-						delivery_estimate: {
-							minimum: {
-								unit: 'business_day',
-								value: 5,
+					line_items,
+					shipping_options: [
+						{
+							shipping_rate_data: {
+								type: 'fixed_amount',
+								fixed_amount: {
+									amount: 1000,
+									currency: 'usd',
+								},
+								display_name: 'Classic Shipping',
+								delivery_estimate: {
+									minimum: {
+										unit: 'business_day',
+										value: 5,
+									},
+									maximum: {
+										unit: 'business_day',
+										value: 7,
+									},
+								},
 							},
-							maximum: {
-								unit: 'business_day',
-								value: 7,
-							},
 						},
+					],
+					mode: 'payment',
+					customer_email: email,
+					success_url: `${process.env.NEXT_PUBLIC_URL}/api/success?orderId=${orderDoc?.id}&token=${token}`,
+					cancel_url: `${process.env.NEXT_PUBLIC_URL}`,
+					metadata: {
+						orderId: orderDoc?.id,
+						token: token,
 					},
 				},
-			],
-			mode: 'payment',
-			customer_email: email,
-			success_url: `${process.env.NEXT_PUBLIC_URL}/api/success?orderId=${orderDoc?.id}&token=${token}`,
-			cancel_url: `${process.env.NEXT_PUBLIC_URL}`,
-			metadata: {
-				orderId: orderDoc?.id,
-				token: token,
-			},
-		});
+			);
 
-		res.json({
-			url: session.url,
-		});
-	} catch {
-		console.error('Error processing checkout:');
-		res
-			.status(500)
-			.json({ error: 'Internal Server Error' });
-	}
+			res.json({
+				url: session.url,
+			});
+		} catch {
+			console.error('Error processing checkout:');
+			res
+				.status(500)
+				.json({ error: 'Internal Server Error' });
+		}
+	} else return;
 }
