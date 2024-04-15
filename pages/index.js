c4789bc2420761120cb4cd70c5635117f0619109
Emmanuel Andy Eze <emmanuel.andyeze@gmail.com>
2024-04-15 12:18:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
 						</span>
 					</p>
 					<p className="text-left italic text-lg mt-2 px-5 md:px-0">
-						+ free shipping!
+						With <span className="text-[#005534] font-bold">free shipping!</span>
 					</p>
 					<div className="hidden md:flex flex-col md:flex-row items-center mt-5 md:mt-6 mb-3 md:mb-0 justify-center md:justify-start">
 						<div className="flex flex-col w-[90%] gap-3 bg-[#fff] p-0 md:p-2 rounded-lg shadow-md text-center mb-5">
